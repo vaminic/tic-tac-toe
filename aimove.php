deeeedd95aca540ac5b6fc47cd6e169dcbb1d73e
D.licious <73747069+vaminic@users.noreply.github.com>
2021-10-14 19:20:21 -0400

    improved AI

--- aimove.php
+++ aimove.php
@@ -229,22 +229,22 @@
 				//Prioritize Corners
 				} elseif ($gridData[0] == "empty" 
 						&& (($gridData[1] == "empty" && $gridData[2] == "empty") 
-						|| ($gridData[1] == "empty" && $gridData[2] == "empty")))
+						|| ($gridData[3] == "empty" && $gridData[6] == "empty")))
 						{
 					echo "#tile1";
 				} elseif ($gridData[2] == "empty" 
 						&& (($gridData[1] == "empty" && $gridData[0] == "empty")
-						|| ($gridData[1] == "empty" && $gridData[0] == "empty")))
+						|| ($gridData[5] == "empty" && $gridData[8] == "empty")))
 						{
 					echo "#tile3";
 				} elseif ($gridData[6] == "empty" 
 						&& (($gridData[7] == "empty" && $gridData[8] == "empty")
-						|| ($gridData[7] == "empty" && $gridData[8] == "empty")))
+						|| ($gridData[0] == "empty" && $gridData[1] == "empty")))
 						{
 					echo "#tile7";
 				} elseif ($gridData[8] == "empty" 
 						&& (($gridData[6] == "empty" && $gridData[7] == "empty")
-						|| ($gridData[6] == "empty" && $gridData[7] == "empty")))
+						|| ($gridData[2] == "empty" && $gridData[5] == "empty")))
 						{
 					echo "#tile9";
 				} elseif ($gridData[7] == "empty" && $gridData[1] == "empty"){
@@ -429,46 +429,30 @@
 	// This function checks the perimer of the grid for possible winning moves
 	function checkPerimeterForWin(){
 		global $gridData;
-		if ($gridData[0] == "O"){
-			if ($gridData[1] == "O" && $gridData[2] == "empty"){
-				return "#tile3";
-			} elseif ($gridData[2] == "O" && $gridData[1] == "empty"){
-				return "#tile2";
-			} elseif ($gridData[3] == "O" && $gridData[6] == "empty"){
-				return "#tile7";
-			} elseif ($gridData[6] == "O" && $gridData[3] == "empty"){
-				return "#tile4";
-			} else {
-				return null;
-			}
-		} elseif ($gridData[2] == "O"){
-			if ($gridData[1] == "O" && $gridData[0] == "empty"){
-				return "#tile1";
-			} elseif ($gridData[5] == "O" && $gridData[8] == "empty"){
-				return "#tile9";
-			} elseif ($gridData[8] == "O" && $gridData[5] == "empty"){
-				return "#tile6";
-			} else {
-				return null;
-			}
-		} elseif ($gridData[6] == "O"){
-			if ($gridData[3] == "O" && $gridData[0] == "empty"){
-				return "#tile1";
-			} elseif ($gridData[7] == "O" && $gridData[8] == "empty"){
-				return "#tile9";
-			} elseif ($gridData[8] == "O" && $gridData[7] == "empty"){
-				return "#tile8";
-			} else {
-				return null;
-			}
-		} elseif ($gridData[8] == "O"){
-			if ($gridData[7] == "O" && $gridData[6] == "empty"){
-				return "#tile7";
-			} elseif ($gridData[5] == "O" && $gridData[2] == "empty"){
-				return "#tile3";
-			} else {
-				return null;
-			}
+		if ($gridData[0] == "O" && $gridData[1] == "O" && $gridData[2] == "empty"){
+			return "#tile3";
+		} elseif ($gridData[0] == "O" && $gridData[2] == "O" && $gridData[1] == "empty"){
+			return "#tile2";
+		} elseif ($gridData[0] == "O" && $gridData[3] == "O" && $gridData[6] == "empty"){
+			return "#tile7";
+		} elseif ($gridData[0] == "O" && $gridData[6] == "O" && $gridData[3] == "empty"){
+			return "#tile4";
+		} elseif ($gridData[2] == "O" && $gridData[1] == "O" && $gridData[0] == "empty"){
+			return "#tile1";
+		} elseif ($gridData[2] == "O" && $gridData[5] == "O" && $gridData[8] == "empty"){
+			return "#tile9";
+		} elseif ($gridData[2] == "O" && $gridData[8] == "O" && $gridData[5] == "empty"){
+			return "#tile6";
+		} elseif ($gridData[6] == "O" && $gridData[3] == "O" && $gridData[0] == "empty"){
+			return "#tile1";
+		} elseif ($gridData[6] == "O" && $gridData[7] == "O" && $gridData[8] == "empty"){
+			return "#tile9";
+		} elseif ($gridData[6] == "O" && $gridData[8] == "O" && $gridData[7] == "empty"){
+			return "#tile8";
+		} elseif ($gridData[8] == "O" && $gridData[7] == "O" && $gridData[6] == "empty"){
+			return "#tile7";
+		} elseif ($gridData[8] == "O" && $gridData[5] == "O" && $gridData[2] == "empty"){
+			return "#tile3";
 		} else {
 			return null;
 		}
@@ -476,46 +460,30 @@
 	// This function checks the perimer of the grid for possible threats
 	function checkPerimeterForThreat(){
 		global $gridData;
-		if ($gridData[0] == "X"){
-			if ($gridData[1] == "X" && $gridData[2] == "empty"){
-				return "#tile3";
-			} elseif ($gridData[2] == "X" && $gridData[1] == "empty"){
-				return "#tile2";
-			} elseif ($gridData[3] == "X" && $gridData[6] == "empty"){
-				return "#tile7";
-			} elseif ($gridData[6] == "X" && $gridData[3] == "empty"){
-				return "#tile4";
-			} else {
-				return null;
-			}
-		} elseif ($gridData[2] == "X"){
-			if ($gridData[1] == "X" && $gridData[0] == "empty"){
-				return "#tile1";
-			} elseif ($gridData[5] == "X" && $gridData[8] == "empty"){
-				return "#tile9";
-			} elseif ($gridData[8] == "X" && $gridData[5] == "empty"){
-				return "#tile6";
-			} else {
-				return null;
-			}
-		} elseif ($gridData[6] == "X"){
-			if ($gridData[3] == "X" && $gridData[0] == "empty"){
-				return "#tile1";
-			} elseif ($gridData[7] == "X" && $gridData[8] == "empty"){
-				return "#tile9";
-			} elseif ($gridData[8] == "X" && $gridData[7] == "empty"){
-				return "#tile8";
-			} else {
-				return null;
-			}
-		} elseif ($gridData[8] == "X"){
-			if ($gridData[7] == "X" && $gridData[6] == "empty"){
-				return "#tile7";
-			} elseif ($gridData[5] == "X" && $gridData[2] == "empty"){
-				return "#tile3";
-			} else {
-				return null;
-			}
+		if ($gridData[0] == "X" && $gridData[1] == "X" && $gridData[2] == "empty"){
+			return "#tile3";
+		} elseif ($gridData[0] == "X" && $gridData[2] == "X" && $gridData[1] == "empty"){
+			return "#tile2";
+		} elseif ($gridData[0] == "X" && $gridData[3] == "X" && $gridData[6] == "empty"){
+			return "#tile7";
+		} elseif ($gridData[0] == "X" && $gridData[6] == "X" && $gridData[3] == "empty"){
+			return "#tile4";
+		} elseif ($gridData[2] == "X" && $gridData[1] == "X" && $gridData[0] == "empty"){
+			return "#tile1";
+		} elseif ($gridData[2] == "X" && $gridData[5] == "X" && $gridData[8] == "empty"){
+			return "#tile9";
+		} elseif ($gridData[2] == "X" && $gridData[8] == "X" && $gridData[5] == "empty"){
+			return "#tile6";
+		} elseif ($gridData[6] == "X" && $gridData[3] == "X" && $gridData[0] == "empty"){
+			return "#tile1";
+		} elseif ($gridData[6] == "X" && $gridData[7] == "X" && $gridData[8] == "empty"){
+			return "#tile9";
+		} elseif ($gridData[6] == "X" && $gridData[8] == "X" && $gridData[7] == "empty"){
+			return "#tile8";
+		} elseif ($gridData[8] == "X" && $gridData[7] == "X" && $gridData[6] == "empty"){
+			return "#tile7";
+		} elseif ($gridData[8] == "X" && $gridData[5] == "X" && $gridData[2] == "empty"){
+			return "#tile3";
 		} else {
 			return null;
 		}
